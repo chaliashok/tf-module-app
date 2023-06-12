5650ac2c239e40fcda35b928e38bce7fa21cd0a4
chaliashok <chali.ashok@gmail.com>
2023-06-12 15:36:39 -0500

    aws parameter setup learning

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "null_resource" "test" {
 provisioner "local-exec" {
   command = "echo Hello World - ${var.env}"
-
-}}
+}
+}
 
 #  connection {
 #    type     = "ssh"
@@ -14,4 +14,4 @@ provisioner "local-exec" {
 #    "sudo labauto ansible",
 #    "ansible-pull -i localhost, -U https://github.com/chaliashok/roboshop-ansible main.yml  -e role_name=${var.name}"
 #  ]
-} }
+#} }
